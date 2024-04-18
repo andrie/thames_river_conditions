a905b7243d6bc46d1f3cecbfea692f467618c6a3
andrie <apdevries@gmail.com>
2024-04-18 18:58:12 +0100

    Update met office API

--- hcc/metoffice.py
+++ hcc/metoffice.py
@@ -4,23 +4,15 @@ import json
 import dotenv
 from typing import Dict, Any, Optional
 import pandas as pd
-# import os
+import os
 
 # call Met Office weatherhub API to retrieve site specific weather data
 # https://api-metoffice.apiconnect.ibmcloud.com/v0
 
 def _decode_response(response: any) -> pd.DataFrame:
-    # resp = json.loads(response.text)
     resp = json.loads(response)
     fcst = resp['features'][0]['properties']['timeSeries']
 
-
-    # loop over fc1 to extract time and temperature
-    for i in range(0, 24):
-        # print(i)
-        print(fcst[i]['time'], fcst[i]['screenTemperature'])
-
-    # convert fc1 to pandas dataframe
     import pandas as pd
     df = pd.DataFrame(fcst)
     df.head()
@@ -31,7 +23,27 @@ def _decode_response(response: any) -> pd.DataFrame:
 
     return(df)
 
-def get_weather(lat:float, lon:float, type:Optional[str] = None) -> pd.DataFrame:
+def get_api_key() -> str:
+    """Get the Met Office API key from the environment variable or .env file"""
+    key = 'MET-OFFICE-API-KEY'
+
+    api_key = None
+    try:
+        api_key = os.environ[key]
+    except KeyError:
+        pass
+    
+    if api_key is None:
+        vars = dotenv.dotenv_values('.env')
+        api_key = vars[key]
+
+    if api_key is None:
+        raise ValueError("MET-OFFICE-API-KEY key not found in environment variable or .env file")
+
+
+    return(api_key)
+
+def get_weather(lat:float, lon:float, type:Optional[str] = None, api_key:Optional[str] = None) -> pd.DataFrame:
     """Get weather forecast data from the Met Office API
 
     :param: lat: latitude
@@ -39,17 +51,23 @@ def get_weather(lat:float, lon:float, type:Optional[str] = None) -> pd.DataFrame
     :param: lon: longitude
 
     :param: type: type of forecast, can be one of "hourly" or "daily". Default is "hourly".
+
+    :param: api_key: Met Office API key. If not provided, it will be read from the environment variable or .env file
     """
     if type is None:
         type = "hourly"
 
     if type not in ["hourly", "daily"]:
         raise ValueError("type must be one of 'hourly' or 'daily'")
+
+    if api_key is None:
+        api_key = get_api_key()
+    
     resp = _call_weather_api(lat, lon, type)
     df = _decode_response(resp)
     return(df)
 
-def _call_weather_api(lat:float, lon:float, type:Optional[str] = None) -> Dict[str, Any]:
+def _call_weather_api(lat:float, lon:float, type:Optional[str] = None, api_key:Optional[str] = None) -> Dict[str, Any]:
     """Get weather forecast data from the Met Office API
 
     :return: a json object with the weather forecast data
@@ -67,25 +85,100 @@ def _call_weather_api(lat:float, lon:float, type:Optional[str] = None) -> Dict[s
         'longitude': lon
     }
 
-    vars = dotenv.dotenv_values('.env')
+    if api_key is None:
+        api_key = get_api_key()
 
     headers = {
         'accept': "application/json", 
-        # MET-OFFICE-ID=b8c778e0d9587cb9bdb57c9d0ba66e92
-        # MET-OFFICE-SECRET=97d660e89a304af1f0deae208b659aba
-        'x-ibm-client-id'     : vars['MET-OFFICE-ID'],
-        'x-ibm-client-secret' : vars['MET-OFFICE-SECRET']
+        'apikey': api_key
         }
 
-    api_url = 'https://api-metoffice.apiconnect.ibmcloud.com/v0/forecasts/point/hourly'
-    # api_url = 'https://api-metoffice.apiconnect.ibmcloud.com/v0/forecasts/point/daily'
-    response = requests.get(api_url, headers = headers, params = params)
-    
-    # check if response is valid
-    if response.status_code != 200:
-        print('Failed to retrieve data:', response.status_code)
-        return None
+    api_url = 'https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point'
+    api_url = api_url + '/' + type
 
-    # fcst = decode_response(response)
+    try:
+        response = requests.get(api_url, headers = headers, params = params)
+        success = True
+    except Exception as e:
+        print('Failed to retrieve data:', e)
+        print('status code:', response.status_code)
+        
+        # stop the program
+        success = False
+
+    
     resp = response.text
     return(resp)
+
+
+weather_codes = {
+'NA':	'Not available',
+'-1':	'Trace rain',
+'0':	'Clear night',
+'1':	'Sunny day',
+'2':	'Partly cloudy (night)',
+'3':	'Partly cloudy (day)',
+'4':	'Not used',
+'5':	'Mist',
+'6':	'Fog',
+'7':	'Cloudy',
+'8':	'Overcast',
+'9':	'Light rain shower (night)',
+'10':	'Light rain shower (day)',
+'11':	'Drizzle',
+'12':	'Light rain',
+'13':	'Heavy rain shower (night)',
+'14':	'Heavy rain shower (day)',
+'15':	'Heavy rain',
+'16':	'Sleet shower (night)',
+'17':	'Sleet shower (day)',
+'18':	'Sleet',
+'19':	'Hail shower (night)',
+'20':	'Hail shower (day)',
+'21':	'Hail',
+'22':	'Light snow shower (night)',
+'23':	'Light snow shower (day)',
+'24':	'Light snow',
+'25':	'Heavy snow shower (night)',
+'26':	'Heavy snow shower (day)',
+'27':	'Heavy snow',
+'28':	'Thunder shower (night)',
+'29':	'Thunder shower (day)',
+'30':	'Thunder'
+}
+
+weather_code_icons = {
+'NA':	'',
+'-1':	'wi-day-rain-mix',
+'0':	'wi-night-clear',
+'1':	'wi-day-sunny',
+'2':	'wi-night-partly-cloudy',
+'3':	'wi-day-cloudy',
+'4':	'',
+'5':	'wi-day-fog',
+'6':	'wi-day-fog',
+'7':	'wi-day-cloudy',
+'8':	'wi-day-sunny-overcast',
+'9':	'wi-night-rain',
+'10':	'wi-day-rain',
+'11':	'wi-day-sleet',
+'12':	'wi-day-rain',
+'13':	'wi-night-storm-showers',
+'14':	'wi-day-storm-showers',
+'15':	'wi-day-showers',
+'16':	'wi-night-sleet-storm',
+'17':	'wi-day-sleet-storm',
+'18':	'wi-day-sleet',
+'19':	'wi-night-hail',
+'20':	'wi-day-hail',
+'21':	'wi-day-hail',
+'22':	'wi-night-snow',
+'23':	'wi-day-snow',
+'24':	'wi-day-snow',
+'25':	'wi-night-snow-thunderstorm',
+'26':	'wi-day-snow-thunderstorm',
+'27':	'wi-day-snow-thunderstorm',
+'28':	'wi-night-thunderstorm',
+'29':	'wi-day-thunderstorm',
+'30':	'wi-day-thunderstorm'
+}
